7df6d982c736248d16d3ceed029bff8cbd7edf2c
Triệu Nguyễn Khánh Băng <tnkbang@cusc.ctu.edu.vn>
2023-01-06 16:00:25 +0900

    Thêm thư viện jquery và đề xuất.

--- Website/wwwroot/js/script.js
+++ Website/wwwroot/js/script.js
@@ -1,38 +1,66 @@
 ﻿
-const navToggler = document.querySelector(".nav-toggler");
-const sidebar = document.querySelector(".sidebar");
-const dropdownBtn = document.querySelector(".drop-down");
-const actionBtn = document.querySelector(".action-btn");
-const dropdown = document.querySelector(".dropdown");
-const profession = document.querySelector("#profession");
-document.addEventListener("DOMContentLoaded", () => {
-    new Typed(profession, options);
-});
-dropdownBtn.addEventListener("click", e => {
-    e.preventDefault();
-    dropdown.classList.toggle("active");
-})
-navToggler.addEventListener("click", () => {
-    sidebar.classList.add("show");
-    navigator.vibrate([50, 100, 50]);
-})
-const hideSidebar = () => {
-    sidebar.classList.remove("show");
-}
-// Amardeep Kesharwani
 const options = {
     strings: ["Good boy", "Web Designer", "Web Developer", "full stack web developer", "Accountant", "Comedian", "Good Advisor", "good boy 😍", "Astrologer"],
     typeSpeed: 100,
     loop: true,
 };
-actionBtn.addEventListener("click", () => {
-    document.querySelector(".action-btn-group").classList.toggle("active");
+
+$(document).on('DOMContentLoaded', () => {
+    const profession = document.querySelector("#profession");
+    new Typed(profession, options);
+})
+
+$('.drop-down').on('click', (e) => {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active');
+})
+
+$('.nav-toggler').on('click', () => {
+    $('.sidebar').addClass('show')
+    navigator.vibrate([50, 100, 50]);
+})
+
+const hideSidebar = () => {
+    $('.sidebar').removeClass('show')
+}
+
+$('.action-btn').on('click', () => {
+    $('.action-btn-group').toggleClass('active');
+})
+
+$('.sidebar_header').on('click', () => {
+    hideSidebar()
+})
+
+$('.overlay').on('click', () => {
+    hideSidebar()
+})
+
+var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+
+$("#tags").autocomplete({
+    source: availableTags
 });
-
-document.querySelector('.sidebar_header').addEventListener('click', () => {
-    hideSidebar()
-})
-
-document.querySelector('.overlay').addEventListener('click', () => {
-    hideSidebar()
-})
